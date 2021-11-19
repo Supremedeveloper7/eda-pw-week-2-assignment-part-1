dfe91ab9a2ea9ac35e0e940760d04cf42c402395
supreme <nurhassan696@gmail.com>
2021-11-19 11:50:06 -0600

    updating homework

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -16,10 +16,10 @@ console.log( 'The lucky number is:', luckyNumber );
 // 6 - Console log this sentence, adding in the variables you created above: 
 // 'My name is (full name), and I think (lucky number) is a winner!'.
 // Refer back to the videos if you need help with this one.
-console.log( 'My name is ' + fullName + ' and i think ' + 7 + ' is a winner' );
+console.log( 'My name is ' + fullName + ' and i think ' + 7 + ' is a winner! ' );
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
 let adventurous = 'True';
-// 8 - Create a variable named `food`, and set its value to a string of your favorite food
+// 8  - Create a variable named `food`, and set its value to a string of your favorite food
 let food = 'Cheesecake';
 console.log( 'My favorite food is:', food );
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
